0874f2cead8882fe2ddc6c77eced37bd95178083
Masonlet <masonletoile@hotmail.com>
2026-01-07 16:37:05 -0500

    Extract bot client creation and startup logic

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits, Events, Collection } = require('discord.js');
-const { loadCommands, registerCommands } = require('./handlers/commandHandler');
-const { setupInteractionHandler } = require('./handlers/eventHandler');
+const { createClient } = require('./bot/client');
+const { setupBot } = require('./bot/setup');
 
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
-client.commands = new Collection();
-
-const commandData = loadCommands(client);
-setupInteractionHandler(client);
-
-client.once(Events.ClientReady, async () => {
-  console.log(`Logged in as ${client.user.tag}`);
-
-  try {
-    console.log('Registering slash commands');
-    await registerCommands(client, commandData);
-  } catch (error) {
-    console.error('Error registering commands:', error);
-  }
-});
+const client = createClient();
+setupBot(client);
 
 client.login(process.env.DISCORD_TOKEN);
