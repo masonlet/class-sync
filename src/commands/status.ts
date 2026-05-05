a5cdce388a69e8109c97b2e05d8204e485649755
Masonlet <masonletoile@hotmail.com>
2026-05-05 00:35:14 -0400

    refactor: convert src/commands/status to typescript

---
+++ src/commands/status.ts
@@ -0,0 +1,15 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
+import { replyEphemeral } from "../utils/interactions";
+
+export const name = "status";
+
+export const data = new SlashCommandBuilder()
+  .setName("status")
+  .setDescription("Check if bot is working")
+  .toJSON();
+
+export async function handle(
+  interaction: ChatInputCommandInteraction
+): Promise<void> {
+  return replyEphemeral(interaction, "Bot is working");
+}
