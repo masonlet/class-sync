466af87f424795d990db0fbc41a0759cd978710c
Masonlet <masonletoile@hotmail.com>
2026-07-07 00:54:09 -0400

    refactor: convert vitest.config.js to typescript

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'node',
-    setupFiles: ['tests/helpers/vitest.setup.js'],
+    setupFiles: ['tests/helpers/vitest.setup.ts'],
   },
 });
